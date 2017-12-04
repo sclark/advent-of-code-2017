d80a5748871a22b2c9fcdb8ca5007a2b535489f6
Stephen Clark <sclark@linux.com>
2017-12-03 23:34:42 -0500

    Solved day 1

---
+++ 1_inverse_captcha/part1.py
@@ -0,0 +1,17 @@
+def solution(captcha):
+
+    total = 0
+    chars = list(captcha)
+
+    for i, c in enumerate(chars):
+        
+        if c == chars[(i+1) % len(chars)]:
+            total += int(c)
+
+    return total
+
+
+if __name__ == "__main__":
+
+    with open("input.txt", "rt") as f:
+        print solution(f.read().strip())
